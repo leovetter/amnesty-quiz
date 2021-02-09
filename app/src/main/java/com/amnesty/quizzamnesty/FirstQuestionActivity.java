4abc4267f5afe370ddfc4e6ee31c487330c087d6
Léo Vetter <vetter.leo@gmail.com>
2021-02-09 21:20:43 +0100

    spinner dropdown ok for first question

--- app/src/main/java/com/amnesty/quizzamnesty/FirstQuestionActivity.java
+++ app/src/main/java/com/amnesty/quizzamnesty/FirstQuestionActivity.java
@@ -4,16 +4,100 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.amnesty.quizzamnesty.views.SubmitArrowView;
 
 public class FirstQuestionActivity extends AppCompatActivity {
+
+    Spinner spinnerRussia = null;
+    Spinner spinnerUsa = null;
+    Spinner spinnerFrance = null;
+    private boolean validated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_question);
+
+        this.spinnerRussia = findViewById(R.id.spinnerRussia);
+        this.spinnerUsa = findViewById(R.id.spinnerUsa);
+        this.spinnerFrance = findViewById(R.id.spinnerFrance);
+
+        //create a list of items for the spinner.
+        String[] items = new String[] {"État Unis", "Royaume-Uni", "Arabie Saoudite", "Russie", "Chine", "France", "Allemagne"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        this.spinnerRussia.setAdapter(adapter);
+        this.spinnerUsa.setAdapter(adapter);
+        this.spinnerFrance.setAdapter(adapter);
+    }
+
+    public void validate(View view) {
+
+        SubmitArrowView submitArrowview = (SubmitArrowView) view;
+        if(!this.validated) {
+            submitArrowview.invalidate();
+
+            if (!this.spinnerRussia.getSelectedItem().toString().equals("Russie")) {
+
+                GradientDrawable border = new GradientDrawable();
+                border.setColor(getResources().getColor(R.color.colorRussia));
+                border.setStroke(4, getResources().getColor(R.color.colorRed));
+                this.spinnerRussia.setBackground(border);
+
+                TextView responseRussia = (TextView) findViewById(R.id.russiaResponse);
+                responseRussia.setText("Russie");
+            } else {
+
+                GradientDrawable border = new GradientDrawable();
+                border.setColor(getResources().getColor(R.color.colorRussia));
+                border.setStroke(4, getResources().getColor(R.color.colorSubmit));
+                this.spinnerRussia.setBackground(border);
+            }
+
+            if (!this.spinnerUsa.getSelectedItem().toString().equals("État Unis")) {
+
+                GradientDrawable border = new GradientDrawable();
+                border.setColor(getResources().getColor(R.color.colorUsa));
+                border.setStroke(4, getResources().getColor(R.color.colorRed));
+                this.spinnerUsa.setBackground(border);
+
+                TextView responseUsa = (TextView) findViewById(R.id.usaResponse);
+                responseUsa.setText("État Unis");
+            } else {
+
+                GradientDrawable border = new GradientDrawable();
+                border.setColor(getResources().getColor(R.color.colorUsa));
+                border.setStroke(4, getResources().getColor(R.color.colorSubmit));
+                this.spinnerUsa.setBackground(border);
+            }
+
+            if (!this.spinnerFrance.getSelectedItem().toString().equals("France")) {
+
+                GradientDrawable border = new GradientDrawable();
+                border.setColor(getResources().getColor(R.color.colorFrance));
+                border.setStroke(4, getResources().getColor(R.color.colorRed));
+                this.spinnerFrance.setBackground(border);
+
+                TextView responseFrance = (TextView) findViewById(R.id.franceResponse);
+                responseFrance.setText("France");
+            } else {
+
+                GradientDrawable border = new GradientDrawable();
+                border.setColor(getResources().getColor(R.color.colorFrance));
+                border.setStroke(4, getResources().getColor(R.color.colorSubmit));
+                this.spinnerFrance.setBackground(border);
+            }
+        }
     }
 
     public void setButtonSelected(View view) {
@@ -27,14 +111,14 @@ public class FirstQuestionActivity extends AppCompatActivity {
 
     public void submitQuestion(View view) {
 
-        View armesLegeresButton = this.findViewById(R.id.button_1);
+        /*View armesLegeresButton = this.findViewById(R.id.button_1);
         View charsButton = this.findViewById(R.id.button_2);
-        View avionsButton = this.findViewById(R.id.button_3);
+        View avionsButton = this.findViewById(R.id.button_3);*/
 
-        MyDatabaseHelper db = new MyDatabaseHelper(this);
+        /*MyDatabaseHelper db = new MyDatabaseHelper(this);
         db.storeResultFirstQuestion(armesLegeresButton.isSelected(), charsButton.isSelected(), avionsButton.isSelected());
 
         Intent intent = new Intent(this, QuizzOrStatsActivity.class);
-        startActivity(intent);
+        startActivity(intent);*/
     }
 }
--- app/src/main/java/com/amnesty/quizzamnesty/views/SubmitArrowView.java
+++ app/src/main/java/com/amnesty/quizzamnesty/views/SubmitArrowView.java
@@ -1,4 +1,83 @@
 package com.amnesty.quizzamnesty.views;
 
-public class SubmitArrowView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.amnesty.quizzamnesty.R;
+
+public class SubmitArrowView extends View {
+
+    Canvas myCanvas = null;
+    private boolean init = true;
+
+    public SubmitArrowView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Log.i("onDraw", "yes yes yes");
+        super.onDraw(canvas);
+
+        this.myCanvas = canvas;
+        if(this.init) {
+            this.drawCircle();
+            this.init = false;
+        } else  {
+            this.drawArrow();
+        }
+    }
+
+    public void drawCircle() {
+
+        Log.i("drawCircle", "yes yes yes");
+
+        Paint background = new Paint();
+        background.setColor(getResources().getColor(R.color.colorSubmit));
+        background.setStrokeWidth(4);
+        background.setStyle(Paint.Style.STROKE);
+        this.myCanvas.drawCircle(getWidth() / 2, getHeight() / 2, 100, background);
+
+        Path path = new Path();
+        path.moveTo(getWidth() / 2 - 40, getHeight() / 2 + 5);
+        path.lineTo(getWidth() / 2, getHeight() / 2 + 50);
+        path.lineTo(getWidth() / 2 + 40, getHeight() / 2 - 40);
+        path.lineTo(getWidth() / 2 + 35, getHeight() / 2 - 45);
+        path.lineTo(getWidth() / 2, getHeight() / 2 + 45);
+        path.lineTo(getWidth() / 2 - 35, getHeight() / 2);
+        path.close();
+        this.myCanvas.drawPath(path, background);
+    }
+
+    public void drawArrow() {
+
+        Log.i("drawArrow", "yes yes yes");
+
+        Paint background = new Paint();
+        background.setColor(getResources().getColor(R.color.colorSubmit));
+        this.myCanvas.drawRect(100, 40, getWidth() - 160, getHeight() - 40, background);
+
+        Path path = new Path();
+        path.moveTo(getWidth() - 160, 0);
+        path.lineTo(getWidth() - 160, getHeight());
+        path.lineTo(getWidth() - 100, getHeight() / 2);
+        path.close();
+        this.myCanvas.drawPath(path, background);
+    }
+
+    public void circleToArrow() {
+
+        Log.i("circleToArrow", this.myCanvas.toString());
+//        this.myCanvas.drawColor(Color.BLACK);
+        this.invalidate();
+        // this.drawArrow();
+    }
+
 }
